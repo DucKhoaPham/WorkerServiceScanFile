5b2e3897d76554b585a9afb87ea5db0667ad5d5b
Khoa <duckhoa@quangich.com>
2023-08-04 11:21:00 +0700

    sửa code

--- Program.cs
+++ Program.cs
@@ -3,90 +3,76 @@ using log4net.Config;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.ServiceProcess;
 using System.IO;
 using System.Reflection;
 using WorkerService.Model;
-using System.Collections;
+using System.Configuration.Install;
+using System;
 
 namespace WorkerService
 {
     public class Program
     {
+        private static readonly ILog _logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         private static ServiceInfo serviceInfo;
         public static void Main(string[] args)
         {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
             ConfigureLogging();
             var builder = CreateHostBuilder(args);
+            IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+            var dirname = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var allConfig = Directory.GetFiles(string.Format("{0}{1}", dirname, @"\Config"), "*.json", SearchOption.AllDirectories);
+            foreach (var jsonFilename in allConfig)
+                if (Path.GetFileName(jsonFilename) == "appsettings.json")
+                {
+                    configurationBuilder.AddJsonFile(jsonFilename);
+                    break;
+                }
+            IConfiguration configuration = configurationBuilder.Build();
+            serviceInfo = configuration.GetSection("ServiceInfo").Get<ServiceInfo>();
             if (args.Length > 0 && args[0].ToLower() == "-i")
-                InstallService();
+                Install();
             else if (args.Length > 0 && args[0].ToLower() == "-u")
-                UninstallService();
+                Uninstall();
             else
                 builder.Build().Run();
         }
         private static void ConfigureLogging()
         {
             var dirname = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            XmlConfigurator.Configure(new FileInfo(string.Format("{0}{1}", dirname, @"\Config\log4net.config")));
+            XmlConfigurator.Configure(new FileInfo(string.Format("{0}{1}", dirname, @"\Config\log4Net.config")));
         }
-        private static void InstallService()
+        private static void Install()
         {
-            using (var serviceProcessInstaller = new ServiceProcessInstaller())
-            {
-                serviceProcessInstaller.Account = ServiceAccount.LocalSystem;
-                using (var serviceInstaller = new ServiceInstaller())
-                {
-                    serviceInstaller.DisplayName = serviceInfo.DisplayName;
-                    serviceInstaller.ServiceName = serviceInfo.ServiceName;
-                    serviceInstaller.StartType = ServiceStartMode.Automatic;
-
-                    var serviceContext = new System.Configuration.Install.InstallContext();
-                    serviceInstaller.Context = serviceContext;
-
-                    var assemblyPath = Assembly.GetExecutingAssembly().Location;
-                    serviceInstaller.Parent = serviceProcessInstaller;
-                    var state = new Hashtable();
-                    serviceInstaller.Install(state);
-                }
-            }
+            ManagedInstallerClass.InstallHelper(new[] { Assembly.GetExecutingAssembly().Location });
+            _logger.InfoFormat("Install successful");
         }
-
-        private static void UninstallService()
+        private static void Uninstall()
         {
-            using (var serviceProcessInstaller = new ServiceProcessInstaller())
-            {
-                serviceProcessInstaller.Account = ServiceAccount.LocalSystem;
-                using (var serviceInstaller = new ServiceInstaller())
-                {
-                    serviceInstaller.ServiceName = serviceInfo.ServiceName;
-
-                    var serviceContext = new System.Configuration.Install.InstallContext();
-                    serviceInstaller.Context = serviceContext;
-
-                    serviceInstaller.Parent = serviceProcessInstaller;
-                    serviceInstaller.Uninstall(null);
-                }
-            }
+            ManagedInstallerClass.InstallHelper(new[] { "/u", Assembly.GetExecutingAssembly().Location });
+            _logger.InfoFormat("Uninstall successful");
         }
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
                     Host.CreateDefaultBuilder(args)
                        .ConfigureAppConfiguration(configurationBuilder =>
-                        {
-                            var dirname = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-                            var allConfig = Directory.GetFiles(string.Format("{0}{1}", dirname, @"\Config"), "*.json", SearchOption.AllDirectories);
-                            foreach (var jsonFilename in allConfig)
-                                configurationBuilder.AddJsonFile(jsonFilename);
-                        }).ConfigureServices((hostContext, services) =>
-                        {
-                            IConfiguration configuration = hostContext.Configuration;
-                            SqlConnectionFactory dbInfo = configuration.GetSection("Database").Get<SqlConnectionFactory>();
-                            services.AddSingleton(dbInfo);
-                            AwsS3Info awsS3Info = configuration.GetSection("AWSS3Info").Get<AwsS3Info>();
-                            services.AddSingleton(awsS3Info);
-                            serviceInfo = configuration.GetSection("ServiceInfo").Get<ServiceInfo>();
-                            services.AddHostedService<TimerWorkerService>();
-                        });
+                       {
+                           var dirname = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                           var allConfig = Directory.GetFiles(string.Format("{0}{1}", dirname, @"\Config"), "*.json", SearchOption.AllDirectories);
+                           foreach (var jsonFilename in allConfig)
+                               configurationBuilder.AddJsonFile(jsonFilename);
+                       }).ConfigureServices((hostContext, services) =>
+                       {
+                           IConfiguration configuration = hostContext.Configuration;
+                           SqlConnectionFactory dbInfo = configuration.GetSection("Database").Get<SqlConnectionFactory>();
+                           services.AddSingleton(dbInfo);
+                           AwsS3Info awsS3Info = configuration.GetSection("AWSS3Info").Get<AwsS3Info>();
+                           services.AddSingleton(awsS3Info);
+                           services.AddHostedService<TimerWorkerService>();
+                       });
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            _logger.Error(e.ExceptionObject.ToString());
+        }
     }
 }
---
+++ VersionUpdateServerInstaller.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.ServiceProcess;
+using WorkerService.Model;
+
+namespace WorkerService
+{
+    [RunInstaller(true)]
+    public partial class VersionUpdateWinSeviceInstaller : System.Configuration.Install.Installer
+    {
+        private ServiceProcessInstaller samplingServiceProcessInstaller;
+
+        private ServiceInstaller samplingServiceInstaller;
+
+        public VersionUpdateWinSeviceInstaller()
+        {
+            InitializeComponent();
+
+            InstallSetup();
+        }
+
+        public void InstallSetup()
+        {
+            IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+            var dirname = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var allConfig = Directory.GetFiles(string.Format("{0}{1}", dirname, @"\Config"), "*.json", SearchOption.AllDirectories);
+            foreach (var jsonFilename in allConfig)
+                if (Path.GetFileName(jsonFilename) == "appsettings.json")
+                {
+                    configurationBuilder.AddJsonFile(jsonFilename);
+                    break;
+                }
+            IConfiguration configuration = configurationBuilder.Build();
+            var serviceInfo = configuration.GetSection("ServiceInfo").Get<ServiceInfo>();
+            this.samplingServiceProcessInstaller = new ServiceProcessInstaller();
+            this.samplingServiceInstaller = new ServiceInstaller();
+            // 
+            // orderEntryServiceProcessInstaller
+            // 
+            this.samplingServiceProcessInstaller.Account = ServiceAccount.LocalSystem;
+            this.samplingServiceProcessInstaller.Password = null;
+            this.samplingServiceProcessInstaller.Username = null;
+            // 
+            // orderEntryServiceInstaller
+            // 
+            this.samplingServiceInstaller.ServiceName = serviceInfo.ServiceName;
+            this.samplingServiceInstaller.DisplayName = serviceInfo.DisplayName;
+            this.samplingServiceInstaller.Description = "Chương trình tự động upload file attachment thư viện";
+            this.samplingServiceInstaller.StartType = ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            Installers.AddRange(new Installer[] 
+            {
+				this.samplingServiceProcessInstaller,
+				this.samplingServiceInstaller
+            });
+        }
+    }
+}
